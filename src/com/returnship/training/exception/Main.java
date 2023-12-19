bd79fc3811bf4d53222a8d9fb396d58c5d8a7cf9
ANKU <ankitadixit1122@gmail.com>
2023-12-18 18:05:30 -0600

    Commit

---
+++ src/com/returnship/training/exception/Main.java
@@ -0,0 +1,29 @@
+package com.returnship.training.exception;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter 5 integer: ");
+            int[] number = new int[10];
+            
+            for(int i=0;i<5;i++){
+  			  number[i] = scanner.nextInt();
+  			  
+  			  	if (number[i] <= 0) {
+  			  		throw new InvalidInputException("Invalid input. Please enter a positive integer.");
+  			  	}
+            
+            	
+            	} 
+          		System.out.println("You enter all positive numbers");
+          		
+        }catch (InvalidInputException e) {     
+             System.out.println("Error: " + e.getMessage());
+             // Catch any other exception and display a generic error message
+        }catch (Exception e) {
+        	System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
